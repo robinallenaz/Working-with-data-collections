6f083d43c9a5aadab2638419aeb73fc51544f261
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-09-16 13:44:36 -0700

    Modified script

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-// Part 3: Feeling Loopy
+const csvString = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
 
 // const csvString =
-//   'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26';
+//   'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232';
 
-const csvString =
-  'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232';
-
-let cell1 = '';
-let cell2 = '';
-let cell3 = '';
-let cell4 = '';
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
 
 let currentCell = 1;
 
 for (let i = 0; i < csvString.length; i++) {
   //checking for new cell
-  if (csvString[i] === ',') {
+  if (csvString[i] === ",") {
     currentCell++;
     continue;
   }
   // checking for a new row
-  if (csvString[i] === '\n') {
-    cell1 = '';
-    cell2 = '';
-    cell3 = '';
-    cell4 = '';
+  if (csvString[i] === "\n") {
+    cell1 = "";
+    cell2 = "";
+    cell3 = "";
+    cell4 = "";
     currentCell = 1;
     continue;
   }
@@ -49,9 +46,22 @@ for (let i = 0; i < csvString.length; i++) {
   }
 
   if (
-    (currentCell === 4 && csvString[i + 1] === '\n') ||
+    (currentCell === 4 && csvString[i + 1] === "\n") ||
     i + 1 === csvString.length
   ) {
     console.log(cell1, cell2, cell3, cell4);
   }
+}
+
+//Part 2
+
+let rows = csvString.split("\n");
+console.log(rows);
+
+//Making array 2 dimensional
+
+let rowArray = [];
+
+for (i = 0; i < rows.length; i++) {
+  let cell = csvString.split(",");
 }
